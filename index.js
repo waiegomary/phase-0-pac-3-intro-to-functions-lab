7a1f1c4a36a5be9c8e7692503d6172e62be35f11
mary_waiyego <waiyegomary55@gmail.com>
2023-03-07 19:18:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 function shout(string) { 
     return string.toUpperCase();
-    
+
+}
+
+function logTwoValues(value1, value2) {
+    console.log(`The two values are ${value1} and ${value2}.`);
+}
+
+
+function sayHelloTo(firstName = "User") {
+    console.log(`Hello, ${firstName}!`);
 }
