944f84be9a0c8f58111ebde369600dbc7fd3ea20
mary_waiyego <waiyegomary55@gmail.com>
2023-03-07 19:30:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -11,3 +11,41 @@ function logTwoValues(value1, value2) {
 function sayHelloTo(firstName = "User") {
     console.log(`Hello, ${firstName}!`);
 }
+
+
+1 + 80; //=> 81
+60 - 40; //=> 20
+2 * 3.4; //=> 6.8
+5.0 / 2.5; //=> 2
+
+
+
+
+number += 3; //=> 8
+number -= 2; //=> 6
+
+
+
+//reset number
+number = 10;
+
+function add5() {
+    return (number += 5);
+}
+
+function divideBy3() {
+    return (number /= 3);
+}
+
+divideBy3(); //=> 3.3333333333333335
+
+add5(); //=> 8.333333333333334
+
+// reset number
+number = 10;
+
+add5(); //=> 15
+
+divideBy3(); //=> 5
+
+parseInt("2.2222", 10);
